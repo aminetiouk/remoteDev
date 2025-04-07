a9a62f7a93a240a9c3d49157161746142274047f
aminetiouk <tiouk.amine@gmail.com>
2025-04-07 21:40:59 +0100

    feat(app): add search state and jobItems fetching logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,36 @@
-import Background from "./Background";
-import Container from "./Container";
-import Footer from "./Footer";
-import Header from "./Header";
+import { useEffect, useState } from 'react';
+import Background from './Background';
+import Container from './Container';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
-  return <>
-    <Background />
+  const [searchText, setSearchText] = useState('');
+  const [jobItems, setJobItems] = useState([]);
 
-    <Header />
+  useEffect(() => {
+    if (!searchText) return;
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+      );
+      const data = await response.json();
+      console.log(data)
+      setJobItems(data.jobItems);
+    };
+    fetchData();
+  }, [searchText]);
+  return (
+    <>
+      <Background />
 
-    <Container />
+      <Header searchText={searchText} setSearchText={setSearchText} />
 
-    <Footer />
-  </>;
+      <Container jobItems={jobItems} />
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
