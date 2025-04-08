23e4887f0fdda44e94959f59fea65b1cd8b5df1c
aminetiouk <tiouk.amine@gmail.com>
2025-04-08 23:59:00 +0100

    feat(app): add isLoading state and pass to JobList for loading indication

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,15 +16,17 @@ import SortingControls from './SortingControls';
 function App() {
   const [searchText, setSearchText] = useState('');
   const [jobItems, setJobItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!searchText) return;
     const fetchData = async () => {
+      setIsLoading(true);
       const response = await fetch(
         `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
       );
       const data = await response.json();
-      console.log(data);
+      setIsLoading(false);
       setJobItems(data.jobItems);
     };
     fetchData();
@@ -49,7 +51,7 @@ function App() {
             <SortingControls />
           </SidebarTop>
 
-          <JobList jobItems={jobItems} />
+          <JobList jobItems={jobItems} isLoading={isLoading} />
           <PaginationControls />
         </Sidebar>
         <JobItemContent />
