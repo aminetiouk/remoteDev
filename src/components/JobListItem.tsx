d086b5b235e81649474bd1d07737c683cfbb9e71
aminetiouk <tiouk.amine@gmail.com>
2025-04-10 23:47:37 +0100

    feat(JobListItem): add href with job ID to support hash navigation

--- src/components/JobListItem.tsx
+++ src/components/JobListItem.tsx
@@ -7,7 +7,7 @@ type TJobItemProp = {
 export default function JobListItem({ jobItem }: TJobItemProp) {
   return (
     <li className="job-item">
-      <a className="job-item__link">
+      <a href={`#${jobItem.id}`} className="job-item__link">
         <div className="job-item__badge">{jobItem.badgeLetters}</div>
 
         <div className="job-item__middle">
