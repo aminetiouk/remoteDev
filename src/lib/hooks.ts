b1c62e69aa514c250f70fbc72764d6a3603a8e51
aminetiouk <tiouk.amine@gmail.com>
2025-04-11 23:28:54 +0100

    feat(hooks): add useJobItem hook and use BASE_API_URL constant

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { JobItem } from './types';
+import { BASE_API_URL } from './constants';
 
 export function useActiveId() {
   const [activeId, setActiveId] = useState<number | null>(null);
@@ -21,6 +22,23 @@ export function useActiveId() {
   return activeId;
 }
 
+export function useJobItem(id: number | null) {
+  const [jobItem, setJobItem] = useState(null);
+  useEffect(() => {
+    if(!id) return;
+
+    const fetchData = async () => {
+      const response = await fetch(`${BASE_API_URL}/${id}`);
+      const data = await response.json();
+      setJobItem(data.jobItem)
+    };
+
+    fetchData();
+  }, [id])
+
+  return jobItem;
+}
+
 export function useJobItems(searchText: string): [JobItem[], boolean] {
   const [jobItems, setJobItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -32,7 +50,7 @@ export function useJobItems(searchText: string): [JobItem[], boolean] {
     const fetchData = async () => {
       setIsLoading(true);
       const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+        `${BASE_API_URL}?search=${searchText}`
       );
       const data = await response.json();
       setIsLoading(false);
