40f4d30458f953137ed1abfcb025ddac92291ddf
aminetiouk <tiouk.amine@gmail.com>
2025-04-13 15:14:53 +0100

    refactor(App): pass total number of search results to ResultsCount

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,7 +16,7 @@ import { useJobItems } from '../lib/hooks';
 
 function App() {
   const [searchText, setSearchText] = useState('');
-  const [jobItems, isLoading] = useJobItems(searchText);
+  const [jobItems, isLoading, totalNumberOfResult] = useJobItems(searchText);
 
   return (
     <>
@@ -34,7 +34,7 @@ function App() {
       <Container>
         <Sidebar>
           <SidebarTop>
-            <ResultsCount />
+            <ResultsCount totalNumberOfResult={totalNumberOfResult} />
             <SortingControls />
           </SidebarTop>
 
