143e0f7c709bee98d8faa20823ec80e8eccd0eaf
aminetiouk <tiouk.amine@gmail.com>
2025-04-13 15:14:53 +0100

    feat(JobList): highlight active job item using useActiveId hook

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,3 +1,4 @@
+import { useActiveId } from '../lib/hooks';
 import { JobItem } from '../lib/types';
 import JobListItem from './JobListItem';
 import Spinner from './Spinner';
@@ -5,15 +6,20 @@ import Spinner from './Spinner';
 type TJobItemsProps = {
   jobItems: JobItem[];
   isLoading: boolean;
-}
+};
 
 export function JobList({ jobItems, isLoading }: TJobItemsProps) {
+  const activeId = useActiveId();
   return (
     <ul className="job-list">
       {isLoading && <Spinner />}
       {!isLoading &&
         jobItems.map(jobItem => (
-          <JobListItem key={jobItem.id} jobItem={jobItem} />
+          <JobListItem
+            key={jobItem.id}
+            jobItem={jobItem}
+            isActive={jobItem.id === activeId}
+          />
         ))}
     </ul>
   );
