d24abe9ddb174e1e7bea16ac45b5f7f1e2214d89
aminetiouk <tiouk.amine@gmail.com>
2025-04-16 19:24:59 +0100

    feat(app): introduce currentPage state and handleChangePage pagination handler

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -23,6 +23,15 @@ function App() {
   const totalNumberOfResult = jobItems?.length;
   const jobItemsSliced = jobItems?.slice(0, 7);
 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleChangePage = (direction: 'next' | 'previous') => {
+    if (direction === 'next') {
+      setCurrentPage(prev => prev + 1);
+    } else if (direction === 'previous') {
+      setCurrentPage(prev => prev - 1);
+    }
+  };
   return (
     <>
       <Background />
@@ -44,7 +53,7 @@ function App() {
           </SidebarTop>
 
           <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
-          <PaginationControls />
+          <PaginationControls  currentPage={currentPage} onClick={handleChangePage} />
         </Sidebar>
         <JobItemContent />
       </Container>
