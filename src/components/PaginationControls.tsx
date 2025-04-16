1a29be0f71cb0f86886b45037467218e42b2ebf3
aminetiouk <tiouk.amine@gmail.com>
2025-04-16 23:29:42 +0100

    refactor(pagination-controls): extract PaginationButton component for cleaner layout and conditional rendering

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,24 +1,60 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons';
 
 type TPaginationControlsProp = {
-  onClick: (direction: "next" | "previous") => void;
+  onClick: (direction: 'next' | 'previous') => void;
   currentPage: number;
-}
+};
 
-export default function PaginationControls({ onClick, currentPage }: TPaginationControlsProp) {
+export default function PaginationControls({
+  onClick,
+  currentPage
+}: TPaginationControlsProp) {
   return (
     <section className="pagination">
-      <button
-        onClick={() => onClick('previous')}
-        className="pagination__button"
-      >
-        <ArrowLeftIcon />
-        page {currentPage === 0 ? 0 : currentPage - 1}
-      </button>
-      <button onClick={() => onClick('next')} className="pagination__button">
-        page {currentPage < 1 ? 1 : currentPage}
-        <ArrowRightIcon />
-      </button>
+      {currentPage > 1 && (
+        <PaginationButton
+          direction="previous"
+          currentPage={currentPage}
+          onClick={() => onClick('previous')}
+        />
+      )}
+      <PaginationButton
+        direction="next"
+        currentPage={currentPage}
+        onClick={() => onClick('next')}
+      />
     </section>
+  );
+}
+
+type PaginationButtonProps = {
+  currentPage: number;
+  onClick: () => void;
+  direction: 'next' | 'previous';
+};
+
+function PaginationButton({
+  currentPage,
+  onClick,
+  direction
+}: PaginationButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`pagination__button pagination__button--${direction}`}
+    >
+      {direction === 'next' && (
+        <>
+          Page {currentPage + 1}
+          <ArrowRightIcon />
+        </>
+      )}
+      {direction === 'previous' && (
+        <>
+          <ArrowLeftIcon />
+          Page {currentPage - 1}
+        </>
+      )}
+    </button>
   );
 }
