cb60f75793ad1e01e5c8a50c505de3dbac5b40b2
aminetiouk <tiouk.amine@gmail.com>
2025-04-16 19:24:59 +0100

    feat(pagination-controls): implement page navigation UI with Arrow icons

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,24 @@
-export default function PaginationControls() {
-  return <section className="pagination"></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons';
+
+type TPaginationControlsProp = {
+  onClick: (direction: "next" | "previous") => void;
+  currentPage: number;
+}
+
+export default function PaginationControls({ onClick, currentPage }: TPaginationControlsProp) {
+  return (
+    <section className="pagination">
+      <button
+        onClick={() => onClick('previous')}
+        className="pagination__button"
+      >
+        <ArrowLeftIcon />
+        page {currentPage === 0 ? 0 : currentPage - 1}
+      </button>
+      <button onClick={() => onClick('next')} className="pagination__button">
+        page {currentPage < 1 ? 1 : currentPage}
+        <ArrowRightIcon />
+      </button>
+    </section>
+  );
 }
