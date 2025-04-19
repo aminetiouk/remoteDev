5e7647f6263818436ad78c89f629a6a2c0926a93
aminetiouk <tiouk.amine@gmail.com>
2025-04-19 22:51:23 +0100

    feat(BookmarkIcon): connect to global BookmarksContext and accept job ID

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,8 +1,15 @@
 import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import { useContext } from "react";
+import { BookmarksContext } from "../contexts/BookmarksContextProvider";
 
-export default function BookmarkIcon() {
+type BookmarkIconProps = {
+  id: number;
+}
+
+export default function BookmarkIcon({id}: BookmarkIconProps) {
+  const {bookmarkedIds, handleToggleBookmark} = useContext(BookmarksContext);
   return (
-    <button className="bookmark-btn">
+    <button className="bookmark-btn" onClick={() => handleToggleBookmark(id)}>
       <BookmarkFilledIcon className="filled" />
     </button>
   );
