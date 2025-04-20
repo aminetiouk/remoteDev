fc294f6b10e1d93a979a73b24a1c600e58ed1778
aminetiouk <tiouk.amine@gmail.com>
2025-04-20 22:39:00 +0100

    fix(BookmarkIcon): prevent propagation and default on toggle click

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,16 +1,25 @@
-import { BookmarkFilledIcon } from "@radix-ui/react-icons";
-import { useContext } from "react";
-import { BookmarksContext } from "../contexts/BookmarksContextProvider";
+import { BookmarkFilledIcon } from '@radix-ui/react-icons';
+import { useContext } from 'react';
+import { BookmarksContext } from '../contexts/BookmarksContextProvider';
 
 type BookmarkIconProps = {
   id: number;
-}
+};
 
-export default function BookmarkIcon({id}: BookmarkIconProps) {
-  const {bookmarkedIds, handleToggleBookmark} = useContext(BookmarksContext);
+export default function BookmarkIcon({ id }: BookmarkIconProps) {
+  const { bookmarkedIds, handleToggleBookmark } = useContext(BookmarksContext);
   return (
-    <button className="bookmark-btn" onClick={() => handleToggleBookmark(id)}>
-      <BookmarkFilledIcon className={`${bookmarkedIds.includes(id) ? "filled" : ""}`} />
+    <button
+      className="bookmark-btn"
+      onClick={e => {
+        handleToggleBookmark(id);
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+    >
+      <BookmarkFilledIcon
+        className={`${bookmarkedIds.includes(id) ? 'filled' : ''}`}
+      />
     </button>
   );
 }
