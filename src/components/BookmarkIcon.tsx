6b02898dba7016cc434d8d349c21500c3aefef4d
aminetiouk <tiouk.amine@gmail.com>
2025-04-22 23:13:06 +0100

    refactor(BookmarkIcon): use useBookmarksContext instead of inline context access

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,19 +1,13 @@
 import { BookmarkFilledIcon } from '@radix-ui/react-icons';
-import { useContext } from 'react';
-import { BookmarksContext } from '../contexts/BookmarksContextProvider';
+import { useBookmarksContext } from '../lib/hooks';
 
 type BookmarkIconProps = {
   id: number;
 };
 
 export default function BookmarkIcon({ id }: BookmarkIconProps) {
-  const context = useContext(BookmarksContext);
-  if(!context) {
-    throw new Error(
-      "useContext(BookmarksContext) must be used within a BookmarksContextProvider"
-    );
-  }
-  const { bookmarkedIds, handleToggleBookmark } = context;
+  const { bookmarkedIds, handleToggleBookmark } = useBookmarksContext();
+
   return (
     <button
       className="bookmark-btn"
