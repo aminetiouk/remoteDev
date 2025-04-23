7ac6eb49a8bd9bb0cbfcff24b8970d6aecaa41d9
aminetiouk <tiouk.amine@gmail.com>
2025-04-23 15:44:57 +0100

    feat(BookmarksButton): add toggleable BookmarksPopover for better UX

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,11 +1,20 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+import BookmarksPopover from "./BookmarksPopover";
 
 export default function BookmarksButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopover = () => {
+    setIsOpen(prev => !prev);
+  };
+
   return (
     <section>
-      <button className="bookmarks-btn">
+      <button onClick={togglePopover} className="bookmarks-btn">
         Bookmarks <TriangleDownIcon />
       </button>
+      { isOpen && <BookmarksPopover/>}
     </section>
   );
 }
