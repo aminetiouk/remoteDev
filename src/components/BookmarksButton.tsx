a98650b246e66c4c9b1728f90f05fd9f640d47d6
aminetiouk <tiouk.amine@gmail.com>
2025-04-26 00:15:19 +0100

    refactor(bookmarks): use useOnClickOutside hook in BookmarksButton for cleaner logic

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,39 +1,25 @@
 import { TriangleDownIcon } from '@radix-ui/react-icons';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import BookmarksPopover from './BookmarksPopover';
+import { useOnClickOutside } from '../lib/hooks';
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
   const popoverRef = useRef<HTMLDivElement>(null);
 
+  useOnClickOutside([buttonRef, popoverRef], () => setIsOpen(false));
+
   const togglePopover = () => {
     setIsOpen(prev => !prev);
   };
-
-  useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
-      if (
-        e.target instanceof HTMLElement &&
-        !buttonRef.current?.contains(e.target) &&
-        !popoverRef.current?.contains(e.target)
-      ) {
-        setIsOpen(false);
-      }
-    };
-    document.addEventListener('click', handleClick);
-
-    return () => {
-      document.removeEventListener('click', handleClick);
-    };
-  }, []);
 
   return (
     <section>
       <button ref={buttonRef} onClick={togglePopover} className="bookmarks-btn">
         Bookmarks <TriangleDownIcon />
       </button>
-      {isOpen && <BookmarksPopover  ref={popoverRef}/>}
+      {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   );
 }
