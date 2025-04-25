f4ab5b990076dae76f18098681d31a6e14bd7a5c
aminetiouk <tiouk.amine@gmail.com>
2025-04-25 15:14:26 +0100

    refactor(button): use refs to handle outside click detection in BookmarksButton

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,9 +1,11 @@
 import { TriangleDownIcon } from '@radix-ui/react-icons';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import BookmarksPopover from './BookmarksPopover';
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const popoverRef = useRef<HTMLDivElement>(null);
 
   const togglePopover = () => {
     setIsOpen(prev => !prev);
@@ -13,8 +15,8 @@ export default function BookmarksButton() {
     const handleClick = (e: MouseEvent) => {
       if (
         e.target instanceof HTMLElement &&
-        !e.target.closest('.bookmarks-btn') &&
-        !e.target.closest('.bookmarks-popover')
+        !buttonRef.current?.contains(e.target) &&
+        !popoverRef.current?.contains(e.target)
       ) {
         setIsOpen(false);
       }
@@ -28,10 +30,10 @@ export default function BookmarksButton() {
 
   return (
     <section>
-      <button onClick={togglePopover} className="bookmarks-btn">
+      <button ref={buttonRef} onClick={togglePopover} className="bookmarks-btn">
         Bookmarks <TriangleDownIcon />
       </button>
-      {isOpen && <BookmarksPopover />}
+      {isOpen && <BookmarksPopover  ref={popoverRef}/>}
     </section>
   );
 }
