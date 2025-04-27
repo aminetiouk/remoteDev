f19854648f053110d7667a028cede4b7246b5ca1
aminetiouk <tiouk.amine@gmail.com>
2025-04-27 23:14:42 +0100

    feat(context): add ActiveIdContextProvider around App

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import BookmarksContextProvider from './contexts/BookmarksContextProvider.tsx';
+import ActiveIdContextProvider from './contexts/ActiveIdContextProvider.tsx';
 
 const queryClient = new QueryClient();
 
@@ -11,7 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BookmarksContextProvider>
-        <App />
+        <ActiveIdContextProvider>
+          <App />
+        </ActiveIdContextProvider>
       </BookmarksContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
