1358477effa5cb1354d04ca8a32ad0a923dcd17a
aminetiouk <tiouk.amine@gmail.com>
2025-04-29 15:30:36 +0100

    feat(context): add JobItemsContextProvider to manage jobs, sorting, and pagination

---
+++ src/contexts/JobItemsContextProvider.tsx
@@ -0,0 +1,72 @@
+import { createContext, useState } from 'react';
+import { useSearchQuery, useSearchTextContext } from '../lib/hooks';
+import { RESULT_PER_PAGE } from '../lib/constants';
+import { JobItem, SortBy } from '../lib/types';
+
+type JobItemsContext = {
+  totalNumberOfResult: number;
+  handleChangePage: (direction: 'next' | 'previous') => void;
+  handleChangeSortBy: (newSortBy: SortBy) => void;
+  jobItemsSortedAndSliced: JobItem[];
+  jobItems: JobItem[] | undefined;
+  isLoading: boolean;
+  sortBy: SortBy;
+  currentPage: number;
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const JobItemsContext = createContext<JobItemsContext | null>(null);
+
+export default function JobItemsContextProvider({ children }: Props) {
+  const { debounceSearchText } = useSearchTextContext();
+  const { jobItems, isLoading } = useSearchQuery(debounceSearchText);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortBy, setSortBy] = useState<SortBy>('relevant');
+
+  const jobItemsSorted = [...(jobItems || [])].sort((a, b) => {
+    if (sortBy === 'relevant') {
+      return b.relevanceScore - a.relevanceScore;
+    } else {
+      return a.daysAgo - b.daysAgo;
+    }
+  });
+
+  const totalNumberOfResult = jobItems?.length;
+  const jobItemsSortedAndSliced = jobItemsSorted.slice(
+    currentPage * RESULT_PER_PAGE - RESULT_PER_PAGE,
+    currentPage * RESULT_PER_PAGE
+  );
+
+  const handleChangePage = (direction: 'next' | 'previous') => {
+    if (direction === 'next') {
+      setCurrentPage(prev => prev + 1);
+    } else if (direction === 'previous') {
+      setCurrentPage(prev => prev - 1);
+    }
+  };
+
+  const handleChangeSortBy = (newSortBy: SortBy) => {
+    setCurrentPage(1);
+    setSortBy(newSortBy);
+  };
+
+  return (
+    <JobItemsContext.Provider
+      value={{
+        totalNumberOfResult,
+        handleChangeSortBy,
+        handleChangePage,
+        jobItemsSortedAndSliced,
+        jobItems,
+        isLoading,
+        sortBy,
+        currentPage
+      }}
+    >
+      {children}
+    </JobItemsContext.Provider>
+  );
+}
