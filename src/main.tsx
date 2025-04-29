0b71cc6a53ce395c3fd8303707ab7b2734734360
aminetiouk <tiouk.amine@gmail.com>
2025-04-29 15:32:44 +0100

    chore(main): wrap App with JobItemsContextProvider in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ActiveIdContextProvider from './contexts/ActiveIdContextProvider.tsx';
 import BookmarksContextProvider from './contexts/BookmarksContextProvider.tsx';
 import SearchTextContextProvider from './contexts/SearchTextContextProvider.tsx';
+import JobItemsContextProvider from './contexts/JobItemsContextProvider.tsx';
 
 const queryClient = new QueryClient();
 
@@ -15,7 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <BookmarksContextProvider>
         <ActiveIdContextProvider>
           <SearchTextContextProvider>
-            <App />
+            <JobItemsContextProvider>
+              <App />
+            </JobItemsContextProvider>
           </SearchTextContextProvider>
         </ActiveIdContextProvider>
       </BookmarksContextProvider>
